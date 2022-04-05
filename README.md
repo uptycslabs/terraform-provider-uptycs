b8f6549be0eda8980ffcf103855f83315d75c453
Marcus Young <myoung34@my.apsu.edu>
2022-04-05 17:58:49 -0500

    update readme, add example

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # Terraform Provider Uptycs
 
+## Auth ##
+
+This provider allows env var auth as well as provider{} auth inline
+
+```
+export UPTYCS_CUSTOMER_ID=your-customer-id
+export UPTYCS_API_SECRET=your-api-secret
+export UPTYCS_API_KEY=your-api-key
+export UPTYCS_HOST=https://test.uptycs.io
+```
+
+Inline:
+
+```
+provider "uptycs" {
+  host = "https://test.uptycs.io"
+  customer_id = "11111111-1111-1111-1111-11111111111"
+  api_key = "2222222222222222222222"
+  api_secret = "234444444444433333333333222222221111111"
+}
+
+
+```
+
 ## Build provider
 
 Run the following command to build the provider
---
+++ examples/alert_rule.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    uptycs = {
+      source = "snooguts.net/reddit/uptycs"
+    }
+  }
+}
+
+provider "uptycs" {
+  host = "https://test.uptycs.io"
+  customer_id = "11111111-1111-1111-1111-11111111111"
+  api_key = "2222222222222222222222"
+  api_secret = "234444444444433333333333222222221111111"
+}
+
+resource "uptycs_alert_rule" "test" {
+  name        = "marcus test 2"
+  description = "marcus test"
+  enabled     = true
+  grouping    = "MITRE"
+  grouping_l2 = "Impact"
+  grouping_l3 = "T1560"
+  sql_config = {
+    interval_seconds : 3600,
+  }
+  code = "test_marc"
+  type = "sql"
+  rule = "select * from processes limit 2 :to;"
+}
